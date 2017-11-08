e050e826846c6244bfd8afefc029571178f903dc
deliriusz <kalinowskirafal.fm@gmail.com>
2017-11-08 21:41:51 +0100

    fixing error handler pushing rest to git

--- controller/register.php
+++ controller/register.php
@@ -6,6 +6,7 @@
  * Time: 22:20
  */
 
+
 class Register
 {
     function view ($f3) {
@@ -14,6 +15,11 @@ class Register
     }
 
     function registerNewUser ($f3) {
-        echo 'posting new user';
+        //$saltLength = random_int(14, 25);
+        phpinfo();
+//        $saltForUser = bin2hex(random_bytes($saltLength));
+        echo 'posting new user    ';
+//        echo 'generated salt     ' . $saltForUser;
+        var_dump($_POST);
     }
 }
