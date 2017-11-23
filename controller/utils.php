83626b80544a831bd5c827dadfb4486b26c1a2fa
deliriusz <kalinowskirafal.fm@gmail.com>
2017-11-23 21:26:23 +0100

    adding new functions to utils

--- controller/utils.php
+++ controller/utils.php
@@ -5,4 +5,17 @@ class Utils {
         $currentTime = time();
         return date($format,$currentTime);
     }
+
+    static function getDateTimeFromTimestamp ($timestamp, $format = "Y-m-d H:i:s") {
+        return date($format, $timestamp);
+    }
+
+    static function getTimestampFromDateTime ($datetimestring, $format = "Y-m-d H:i:s") {
+        $dtime = DateTime::createFromFormat($format, $datetimestring);
+        return $dtime->getTimestamp();
+    }
+
+    static function getDateTimeDifference ($from, $to) {
+        return self::getTimestampFromDateTime($from) - self::getTimestampFromDateTime($to);
+    }
 }
