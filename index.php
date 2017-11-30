fa9dc65050668c39591eabf3551b3abf868f5e45
Rafal Kalinowski <kalinowskirafal.fm@gmail.com>
2017-11-30 16:54:16 +0100

    - working on index page- still results in an error

--- index.php
+++ index.php
@@ -13,10 +13,21 @@
 		$f3->get('dbuser'),
 		$f3->get('dbpassword')) );
 
-    //new DB\SQL\Session($f3->get('DB')); 
+    //new DB\SQL\Session($f3->get('DB'));
 
     $f3->route ('GET /',
         function ($f3) {
+					// 'order'=>'userID DESC',
+// 'offset'=>5,
+// 'limit'=>3
+$jobsController = new Jobs ();
+$jobs = $jobsController->getJobs (
+	NULL, array (
+		'order' => 'initial_price DESC',
+		'limit' => 5
+		)
+);
+$f3->set('jobs', $jobs);
             $f3->set('content', 'start.html');
             echo Template::instance()->render('template.html');
         }
